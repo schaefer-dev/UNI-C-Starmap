b917385a77e23379b4d07c094b7e98087bba9fb8
Daniel Schäfer <s9dlscae@stud.uni-saarland.de>
2014-05-22 17:01:39 +0200

    star read working

--- star.c
+++ star.c
@@ -6,8 +6,18 @@
 
 int star_read_from_file(struct star* s, FILE* f)
 {
-    // TODO implement	
-	abort();
+    double xcord, ycord, zcord, mag;
+    int drapnr, hrn;
+	if (6==fscanf(f, "%lf %lf %lf %i %lf %i",&xcord,&ycord,&zcord,&drapnr,&mag,&hrn)){
+        s->x = xcord;
+        s->y = ycord;
+        s->magnitude = mag;
+        s->draper=drapnr;
+        return 1;
+    }
+    else
+        return 0;
+
 }
 
 void star_coord_to_pixel(struct star const* s, struct image* const img, int* x, int* y)
