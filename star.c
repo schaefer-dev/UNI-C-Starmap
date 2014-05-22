9a19eb606126226f69a2a4f5a5cd85fd821e670d
Daniel Schäfer <s9dlscae@stud.uni-saarland.de>
2014-05-22 16:21:54 +0200

    coord to pixel working

--- star.c
+++ star.c
@@ -6,14 +6,24 @@
 
 int star_read_from_file(struct star* s, FILE* f)
 {
-	// TODO implement
+    // TODO implement	
 	abort();
 }
 
 void star_coord_to_pixel(struct star const* s, struct image* const img, int* x, int* y)
 {
-	// TODO implement
-	abort();
+	double a = s->x;
+    double b = s->y;
+    int height = img->h;
+    int width = img->w;
+    double mheight=(((double)height)-1)/2.0;
+    double mwidth=(((double)width)-1)/2.0;
+
+    *x =(int)((1.0+a)*mwidth);
+
+    *y = (int)((1.0-b)*mheight);
+
+
 }
 
 void star_plot(struct star const* s, struct image* img)
